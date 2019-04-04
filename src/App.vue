afa6204aabaa5ea6a5c4a77ebce145d1f2fba97b
SimasDei <simondeiman@gmail.com>
2019-04-04 22:25:59 +0300

    Component creation, handling props with data

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,30 @@
 <template>
   <div id="app">
+    <Header :title="title"/>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import Header from "./components/Header.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    Header
+  },
+  data: () => ({
+    title: "Vue Movie DB"
+  })
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header>
+    <h1>{{title}}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {
+    title: String
+  }
+};
+</script>
+
+<style scoped>
+header {
+  background-color: #111;
+  padding: 20p;
+  color: #fff;
+}
+h1 {
+  margin: 0;
+}
+</style>
